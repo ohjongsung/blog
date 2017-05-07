275bfd8ce4029ef1aad5f26983fb43055dd9528f
ohjongsung <ohjongsung@gmail.com>
2017-05-07 18:07:16 +0900

    html parser for post summary

---
+++ src/main/java/io/ohjongsung/blog/PostSummary.java
@@ -0,0 +1,29 @@
+package io.ohjongsung.blog;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ohjongsung on 2017-05-07. HTML 을 파싱해서 포스트 내용 요약을 만든다.
+ */
+@Service
+public class PostSummary {
+
+    public String forContent(String content, int maxLength) {
+        Document document = Jsoup.parse(content);
+        StringBuilder builder = new StringBuilder();
+        int count = 0;
+        for (Element element : document.body().children()) {
+            builder.append(element.outerHtml());
+            builder.append("\n");
+            count += element.text().length();
+            if (count >= maxLength) {
+                break;
+            }
+        }
+
+        return builder.toString();
+    }
+}
