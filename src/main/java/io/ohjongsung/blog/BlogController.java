1a61abc8665edf20b9c65374241b4137f9397798
ohjongsung <ohjongsung@gmail.com>
2017-05-09 12:41:30 +0900

    카테고리에 따른 진입 리퀘스트 매핑 추가
    전체 글 또는 카테고리 선택에 따른 RequestMapping 으로 세분화 했다. 그리고 공통
    적인 내용을 따로 renderListOfPosts method 로 공통화 했다.

--- src/main/java/io/ohjongsung/blog/BlogController.java
+++ src/main/java/io/ohjongsung/blog/BlogController.java
@@ -3,13 +3,23 @@ package io.ohjongsung.blog;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
 
+import io.ohjongsung.blog.entity.Post;
+import io.ohjongsung.blog.support.PostCategory;
+import io.ohjongsung.blog.support.PostView;
+import io.ohjongsung.support.nav.PageableFactory;
+import io.ohjongsung.support.nav.PaginationInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import io.ohjongsung.support.DateFactory;
+
+import java.util.List;
 
 /**
  * Created by ohjongsung on 2017-05-08.
@@ -28,7 +38,26 @@ public class BlogController {
 
     @RequestMapping(value = "", method = { GET, HEAD })
     public String listPublishedPosts(Model model, @RequestParam(defaultValue = "1") int page) {
+        Pageable pageRequest = PageableFactory.forLists(page);
+        Page<Post> result = service.getPublishedPosts(pageRequest);
+        return renderListOfPosts(result, model, "All Posts");
+    }
 
+    @RequestMapping(value = "/category/{category}", method = { GET, HEAD })
+    public String listPublishedPostsForCategory(@PathVariable("category") PostCategory category, Model model,
+                                                @RequestParam(defaultValue = "1", value = "page") int page) {
+        Pageable pageRequest = PageableFactory.forLists(page);
+        Page<Post> result = service.getPublishedPosts(category, pageRequest);
+        return renderListOfPosts(result, model, category.getDisplayName());
+    }
+
+    private String renderListOfPosts(Page<Post> page, Model model, String activeCategory) {
+        Page<PostView> postViewPage = PostView.pageOf(page, dateFactory);
+        List<PostView> posts = postViewPage.getContent();
+        model.addAttribute("activeCategory", activeCategory);
+        model.addAttribute("categories", PostCategory.values());
+        model.addAttribute("posts", posts);
+        model.addAttribute("paginationInfo", new PaginationInfo(postViewPage));
         return "index";
     }
 }
