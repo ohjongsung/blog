80ec0923737c511fb249373b7cd394e0f3b42aad
ohjongsung <ohjongsung@gmail.com>
2017-05-13 21:06:49 +0900

    Filter 추가
    <input type="hidden" name="_method" value="DELETE"/> 다음과 같은 Hidden
    Input 을 사용하기 위해서는 HiddenHttpMethodFilter 를 사용해야 한다.

--- src/main/java/io/ohjongsung/WebInitializer.java
+++ src/main/java/io/ohjongsung/WebInitializer.java
@@ -1,7 +1,9 @@
 package io.ohjongsung;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
@@ -22,6 +24,10 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
     @Override
     protected String[] getServletMappings() {
         return new String[] { "/" };
+    }
+
+    @Override protected Filter[] getServletFilters() {
+        return new Filter[] { new HiddenHttpMethodFilter() };
     }
 
     @Override public void onStartup(ServletContext servletContext) throws ServletException {
