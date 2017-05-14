8e09bcd5beed34f2d7935e94ee475c268be39f6c
ohjongsung <ohjongsung@gmail.com>
2017-05-14 11:45:13 +0900

    SpringSecurityFilter 등록을 위한 생성
    요걸 만들어줘야 시큐리티 필터가 등록된다.

---
+++ src/main/java/io/ohjongsung/BlogSecurityInitializer.java
@@ -0,0 +1,9 @@
+package io.ohjongsung;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by ohjongsung on 2017-05-13.
+ */
+public class BlogSecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+}
